5229fe3552722607d6f1027fd184dac211ed45b7
humberto1312 <162592407+humberto1312@users.noreply.github.com>
2024-03-24 16:18:22 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Imprime os números de 1 a 20 um abaixo do outro
+        System.out.println("Números de 1 a 20 um abaixo do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // Imprime os números de 1 a 20 um ao lado do outro
+        System.out.println("\nNúmeros de 1 a 20 um ao lado do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+     
+      
+
+
+
+
+
+
